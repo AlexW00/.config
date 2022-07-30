7833f23e6851f855116e2fd7b4117bc83105db6d
Alexander Weichart <alexanderweichart@icloud.com>
2022-07-30 21:04:12 +0200

    install scripts

---
+++ install_packages.sh
@@ -0,0 +1,9 @@
+# installs all the necessary packages
+
+# antigen
+curl -L git.io/antigen > /usr/share/zsh/share/antigen.zsh
+
+# normal packages using pacman
+wget https://raw.githubusercontent.com/AlexW00/.config/master/packages
+sudo pacman -S --needed - < packages
+rm -rf packages
--- setup.sh
+++ setup.sh
@@ -1,8 +1,24 @@
-# Setup script to symlink all config files
+#!/usr/bin/env bash 
+
+# rename old config
+mv "$HOME/.config" "$HOME/.config_old"
+
+# clone new config
+git clone https://github.com/AlexW00/.config.git "$HOME/"
+
+# add files from old config folder to new config
+cp -n -a "$HOME/.config_old/." "$HOME/.config"
 
 # symlinks
 ln -s "$HOME/.config/zsh/.zshrc" "$HOME/.zshrc"
 
-# finish
+read -p "Do you wish to install the packages as well (ONLY ARCH LINUX)? <y/N> " prompt
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+then
+    wget https://raw.githubusercontent.com/AlexW00/.config/master/install_packages.sh
+else
+    echo "Finished setup (with package installation)"
+fi
 
-echo "Finished setup"
+
+echo "Finished setup (withut package installation)"
