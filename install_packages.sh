bd3d855e3c328597e45b518592b9cd937d588036
Alexander Weichart <alexanderweichart@icloud.com>
2022-07-30 21:26:53 +0200

    comment out

--- install_packages.sh
+++ install_packages.sh
@@ -1,7 +1,7 @@
 # installs all the necessary packages
 
 # antigen
-curl -L git.io/antigen > /usr/share/zsh/share/antigen.zsh
+#curl -L git.io/antigen > /usr/share/zsh/share/antigen.zsh
 
 # normal packages using pacman
 wget https://raw.githubusercontent.com/AlexW00/.config/master/packages
