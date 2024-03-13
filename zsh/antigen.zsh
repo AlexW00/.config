d78d7bfcfb1f97fa999b5cf06e0c9b9bcada2e9b
Alexander <alexanderweichart@fastmail.com>
2024-03-13 07:47:30 +0100

    Mi 13. Mär 07:47:30 CET 2024

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -6,7 +6,8 @@ antigen bundle copypath
 antigen bundle zsh-users/zsh-syntax-highlighting
 # auto update antigen
 # antigen bundle unixorn/autoupdate-antigen.zshplugin
-
+antigen bundle Aloxaf/fzf-tab
+zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
 # Theme
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle johanhaleby/kubetail
