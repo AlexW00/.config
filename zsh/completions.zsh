5a6e4c9760a5f7d982987a43896c96321a497f0a
Alexander Weichart <alexander.weichart@debtvision.de>
2024-03-26 14:29:01 +0100

    Tue Mar 26 14:29:01 CET 2024

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -11,16 +11,21 @@ fi
 function create_completion_if_not_exists() {
     if [ ! -f $1 ]; then
         echo "Creating completion file: $1"
-        $2 completion zsh > $1
+        $2 $3 zsh > $1
     fi
 }
 
 function load_completion() {
     local type=$1
     local file="$COMPLETION_DIR/$type"
+    local completion_command="completion"
+
+    if [[ -n $2 ]]; then
+        completion_command=$2
+    fi
 
     if type $1 > /dev/null 2>&1; then
-        create_completion_if_not_exists $file $1
+        create_completion_if_not_exists $file $1 $completion_command
         source $file
     fi
 }
@@ -29,7 +34,9 @@ load_completion kubectl
 load_completion flux
 load_completion helm
 load_completion docker
+load_completion talosctl
 load_completion op # doesnt work idk
+load_completion yq "shell-completion"
 
 function delete_all_completions() {
     rm -rf $COMPLETION_DIR/*
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,5 +1,6 @@
 local plugins_dir=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/
 
+
 function install_plugin() {
     local plugin_name=$1
     local plugin_url=$2
@@ -7,6 +8,10 @@ function install_plugin() {
     if [ ! -d "$plugins_dir/$plugin_name" ]; then
         echo "Cloning $plugin_name"
         git clone $plugin_url ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/$plugin_name
+    fi
+
+    if [[ ! " ${plugins[@]} " =~ " ${plugin_name} " ]]; then
+        plugins+=($plugin_name)
     fi
 }
 
@@ -19,10 +24,25 @@ function remove_plugin() {
     fi
 }
 
+function clean_plugins() {
+    for plugin in $plugins_dir/*; do
+        # if not in the list of plugins
+        if [[ ! " ${plugins[@]} " =~ " ${plugin##*/} " ]]; then
+            remove_plugin ${plugin##*/}
+        fi
+    done
+}
+
 install_plugin "fzf-tab" "https://github.com/Aloxaf/fzf-tab"
 install_plugin "zsh-completions" "https://github.com/zsh-users/zsh-completions"
 install_plugin "zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions"
+install_plugin "zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting"
 install_plugin "ansible-server" "https://github.com/viasite-ansible/zsh-ansible-server.git"
+install_plugin "jq" "https://github.com/reegnz/jq-zsh-plugin"
 
-plugins=(fzf-tab zsh-completions zsh-autosuggestions ansible-server)
 zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+
+# bind to enter
+bindkey '^e' jq-complete
+
+clean_plugins
