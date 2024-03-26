0dd9bfd85b0051ac56769d698dcda8f7a576f0e8
Alexander Weichart <alexander.weichart@debtvision.de>
2024-03-26 14:43:25 +0100

    Tue Mar 26 14:43:25 CET 2024

--- zsh/aliases/toolbox.zsh
+++ zsh/aliases/toolbox.zsh
@@ -1,16 +1,16 @@
 # Fedora Toolbox
-alias tbe="toolbox enter"
-alias tbr="toolbox run"
-alias tbc="toolbox create"
-alias tbls="toolbox list"
-alias tbrm="toolbox rm"
+# alias tbe="toolbox enter"
+# alias tbr="toolbox run"
+# alias tbc="toolbox create"
+# alias tbls="toolbox list"
+# alias tbrm="toolbox rm"
 
-# Quick Runs
-alias u="toolbox run -c u"
-alias howto="u ai ask"
-alias onefetch="u onefetch"
+# # Quick Runs
+# alias u="toolbox run -c u"
+# alias howto="u ai ask"
+# alias onefetch="u onefetch"
 
-alias p="toolbox run -c p"
+# alias p="toolbox run -c p"
 
-alias t="toolbox run -c t"
-alias mods="t mods"
+# alias t="toolbox run -c t"
+# alias mods="t mods"
