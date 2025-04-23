5bf4d205287bae62cd6683ea44a8d2aab55cd912
Alexander Weichart <alexanderweichart@fastmail.com>
2025-04-23 18:48:43 +0200

    rm nvm

--- zsh/aliases/programs.zsh
+++ zsh/aliases/programs.zsh
@@ -6,13 +6,6 @@ alias kj="java -jar $HOME/Programs/Java/KanjiTomo/KanjiTomo.jar -run"
 # Alt names
 alias gitfetch="onefetch"
 
-# NVM
-NVM_DIR="$HOME/.nvm"
-# This loads nvm
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# This loads nvm bash_completion
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
 # if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
 #    tmux new
 # fi
